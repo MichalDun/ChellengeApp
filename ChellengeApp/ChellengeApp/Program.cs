e2a1ac13127a7c3daa1ba8073d82ccbdd9a4d23f
MichalDun <michaldunajewski@gmail.com>
2023-09-14 01:11:14 +0200

    Zadanie dzień 3

--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Witajcie, jak na razie bez większych przeszkód");
+﻿var name = "Ewa";
+bool Kobieta = false;
+var age = 17;
+
+if (Kobieta == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta w wieku powyżej 29 lat");
+    }
+    if (age == 33)
+    {
+        if (name == "Ewa")
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+}
+
