7b7d49c6af70d12c546e5c93df97ed9833e00171
MichalDun <michaldunajewski@gmail.com>
2023-10-06 21:57:22 +0200

    Zadanie dzień 10

--- ChellengeApp/ChellengeApp/Employee.cs
+++ ChellengeApp/ChellengeApp/Employee.cs
@@ -12,16 +12,52 @@ namespace ChellengeApp
             this.Surname = surname;
 
         }
-
-
         public string Name { get; private set; }
 
         public string Surname { get; private set; }
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Value");
+            }
         }
+
+        public void AddGrade(int grade)
+        {
+            float value = grade;
+            this.AddGrade(value);
+        }
+        public void AddGrade(double grade)
+        {
+            float value = (float)grade;
+            this.AddGrade(value);
+        }
+
+        public void AddGrade(long grade)
+        {
+            float value = (float)grade;
+            this.AddGrade(value);
+        }
+
+        public void AddGrade(string grade)
+        {
+           if (float.TryParse(grade, out float result))
+           {
+               this.AddGrade(result);
+           }
+            else
+            {
+               Console.WriteLine("This string is not a float");
+            }
+
+        }
+
 
         public Statistics GetStatistics()
         {
--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -3,7 +3,11 @@
 var employee = new Employee("Michał", "Dunajewski");
 employee.AddGrade(2);
 employee.AddGrade(5);
-employee.AddGrade(3);
+employee.AddGrade(1.5);
+employee.AddGrade(290);
+employee.AddGrade("8,3");
+employee.AddGrade(5.7F);
+employee.AddGrade(0x2F);
 var statistics = employee.GetStatistics();
 
 Console.WriteLine($"Avarage: {statistics.Average:N2}");
